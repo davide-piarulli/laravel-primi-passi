1804ab96265d81d6cfdd05c26ea1a022fb77eb9c
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-08 13:14:18 +0100

    [add] hello world

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css'
+        integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ=='
+        crossorigin='anonymous' />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container my-5 ">
+        <h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos corporis, illum nesciunt ab exercitationem
+            minima accusantium ipsam, minus natus eum officiis quaerat ut culpa debitis dolore mollitia eius! Placeat,
+            rerum.</p>
+    </div>
+</body>
+
+</html>
